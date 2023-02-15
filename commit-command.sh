ea084d7b57764abee54db9ef47efd43b667cc8e3
sru2210 <sru221099@gmail.com>
2023-02-15 11:52:01 +0000

    commit-3

--- commit-command.sh
+++ commit-command.sh
@@ -1,7 +1,6 @@
 cat <<EOF >.git/hooks/post-commit
 git push
 git log -1 --shortstat > history_log.txt
-mvn test >testCases_log.txt
 EOF
 chmod +x .git/hooks/post-commit
 cat <<EOF >.git/hooks/pre-commit
